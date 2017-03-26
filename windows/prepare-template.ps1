4f0642d812c9975f3a8f61532a59f3fcbc5690d5
Tomaz Cerar <tomaz.cerar@gmail.com>
2017-03-26 20:37:01 +0200

    add windows setup scripts

---
+++ windows/prepare-template.ps1
@@ -0,0 +1,17 @@
+﻿# Prepare windows host for templating
+# -----------------------------------------------------------
+#
+
+Disable-PSRemoting -Force
+Get-ChildItem WSMan:\localhost\Listener\* | Remove-Item -Recurse
+Get-ChildItem -Path Cert:\LocalMachine\My | Remove-Item
+
+Stop-Service -Name "TCBuildAgent"
+rm -Force -Recurse -ErrorAction SilentlyContinue -Path C:\BuildAgent\tools 
+rm -Force -Recurse -ErrorAction SilentlyContinue -Path C:\BuildAgent\work
+rm -Force -Recurse -ErrorAction SilentlyContinue -Path C:\BuildAgent\logs
+rm -Force -Recurse -ErrorAction SilentlyContinue -Path C:\BuildAgent\temp
+rm -Force -Recurse -ErrorAction SilentlyContinue -Path C:\BuildAgent\plugins
+
+$content = Get-Content -path "C:\BuildAgent\conf\buildAgent.properties" | select-string -pattern 'authorizationToken' -notmatch
+Set-Content -Value $content -Path "C:\BuildAgent\conf\buildAgent.properties"
